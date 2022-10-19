f76858cef44300b512c0b55af00e50ae33359b73
simonszabo <simimi129@gmail.com>
2022-10-19 22:22:16 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Simon Szabo on 2022. 10. 19..
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
